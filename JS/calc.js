ae16544a699138efc0a3be807f70de27154ba8f0
nathan.seganti <nathan.seganti@gmail.com>
2025-10-22 15:37:56 +0200

    Definita forma generale Calc

---
+++ JS/calc.js
@@ -0,0 +1,23 @@
+let result = 0;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function mult(a, b){
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+const add = document.getElementById("1");
+const sub = document.getElementById("2");
+const mult = document.getElementById("3");
+const div = document.getElementById("4");
+
