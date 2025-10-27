4a90ba13e9d8d5cb838b47830bea33b4f433f509
nathan.seganti <nathan.seganti@gmail.com>
2025-10-27 16:22:42 +0100

    fix load + fix log res

--- JS/calc.js
+++ JS/calc.js
@@ -7,6 +7,8 @@ let val2 = document.getElementById("val2");
 
 let result = document.getElementById("result");
 
+let memory = document.getElementById("memory");
+
 
 //Getting the input from the button and printing it
 //in the console to get a feedback
@@ -32,7 +34,21 @@ allBtn.forEach((btn) => {
             result.innerText = "Error " + res.message;
         }else{
             result.innerText = "Result " + res;
-        } 
+            
+            let myLog = new MyLog(a, b, operation);
+            //creating list for log of the inner memory
+            let btnLoad = document.createElement("button");
+            btnLoad.innerText = "LOAD";
+            btnLoad.addEventListener("click", myLog.fnLoad);
+            btnLoad.className += "btnLoad";
+
+            let logLine = document.createElement("li");
+            logLine.innerText = `Eseguito ${myLog.val1} ${myLog.operation} ${myLog.val2}`;
+            logLine.append(btnLoad);
+
+            memory.appendChild(logLine);
+        }
+        console.log(result); 
     });
 });
 
@@ -77,5 +93,18 @@ function div(a, b){
     return a / b;
 }
 
+
+//AGGIUNTA MEMORIA INTERNA
+function MyLog(a, b, operation){
+    this.val1 = a;
+    this.val2 = b;
+    this.operation = operation;
+
+    this.fnLoad = () => {
+        console.log("LOAD", this.val1, this.val2);
+        val1.value = this.val1;
+        val2.value = this.val2;
+    }
+} 
 
 
